26385dfc634f003664086ac0032d270c7222bcf0
ghostcode-sys <prashantchandel2103@gmail.com>
2021-12-15 09:08:42 +0530

    index.js id changed

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ app.use("/easy", FormRender);
 
 if(process.env.NODE_ENV = "production"){
   app.use(express.static("frontend/build"))
+  const path = require("path");
+
+    app.get("*", (req, res) => {
+
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+
+    }
 }
 
 const getData = async (id) => {
